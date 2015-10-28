aff6e36596e2f0edc0d2eea6bda9088216cee50e
“Xuchen1591” <magina.1591@outlook.com>
2015-10-28 22:20:51 +0800

    jQuery lookThrough

--- JS/maginaJQHTML.js
+++ JS/maginaJQHTML.js
@@ -77,6 +77,12 @@ $(document).ready(function(){
        $(".addClass").addClass("important");
         $("#addB").addClass("blue");
     });
+
+
+    //METHOD list
+    //parent()      返回被选元素的直接父元素
+    //parents()     返回被选元素的所有祖先元素，它一路向上直到文档的根元素 (<html>)
+    //parentsUntil()        返回介于两个给定元素之间的所有祖先元素
 });
 
 
