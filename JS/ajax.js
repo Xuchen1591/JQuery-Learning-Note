e625ce5c3ef9529071e30396b8c900874ba9b3c5
“Xuchen1591” <magina.1591@outlook.com>
2015-10-29 23:10:50 +0800

    response Text & XML parse

--- JS/ajax.js
+++ JS/ajax.js
@@ -3,6 +3,8 @@
  */
 function loadXMLDoc(){
     var xmlhttp;
+    var txt, i, x,xmlDoc;
+
     if (window.XMLHttpRequest)
     {// code for IE7+, Firefox, Chrome, Opera, Safari
         xmlhttp=new XMLHttpRequest();
@@ -15,10 +17,22 @@ function loadXMLDoc(){
     {
         if (xmlhttp.readyState==4 && xmlhttp.status==200)
         {
-            document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
+            //responseText() used for none XML file, return String type
+            //document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
+
+            //responseXML used for XML file, u can parse the XML
+            xmlDoc=xmlhttp.responseXML;
+            txt="";
+            x=xmlDoc.getElementsByTagName("title");
+            for (i=0;i<x.length;i++)
+            {
+                txt=txt + x[i].childNodes[0].nodeValue + "<br />";
+            }
+            document.getElementById("myDiv").innerHTML=txt;
+
         }
     }
-    xmlhttp.open("GET","/Jquery Learn Note/testContent/test.txt",true);
+    xmlhttp.open("GET","/Jquery Learn Note/testContent/books.xml",true);
     xmlhttp.send();
 
     //规定请求的类型、URL 以及是否异步处理请求。
